43c907b930751f75f4acc097c6902f31adb85cd9
LTE04 <leetungern@graduate.utm.my>
2026-04-15 10:28:48 +0800

    create taskcard

--- taskmanager.js
+++ taskmanager.js
@@ -23,3 +23,69 @@ addButtons.forEach(btn => {
 cancelBtn.addEventListener('click', function () {
     modal.classList.add('hidden');
 });
+
+// create task objek and save
+saveBtn.addEventListener('click', function () {
+
+    const title = document.getElementById('task-title').value;
+    const desc = document.getElementById('task-desc').value;
+    const priority = document.getElementById('task-priority').value;
+    const date = document.getElementById('task-date').value;
+
+    if (!title) return;
+
+    const task = {
+        id: currentId++,
+        title: title,
+        description: desc,
+        priority: priority,
+        date: date,
+        column: currentColumn
+    };
+
+    tasks.push(task);
+
+    addTask(currentColumn, task);
+
+    modal.classList.add('hidden');
+});
+
+//create taskCard
+function createTaskCard(task) {
+
+    const li = document.createElement('li');
+    li.setAttribute('data-id', task.id);
+    li.classList.add('task-card');
+
+    const title = document.createElement('span');
+    title.textContent = task.title;
+    li.appendChild(title);
+
+    const desc = document.createElement('p');
+    desc.textContent = task.description;
+    li.appendChild(desc);
+
+    const prio = document.createElement('span');
+    prio.textContent = "Priority: " + task.priority;
+    li.appendChild(prio);
+
+    const date = document.createElement('p');
+    date.textContent = task.date;
+    li.appendChild(date);
+
+    // Edit Button
+    const editBtn = document.createElement('button');
+    editBtn.textContent = "Edit";
+    editBtn.setAttribute('data-action', 'edit');
+    editBtn.setAttribute('data-id', task.id);
+    li.appendChild(editBtn);
+
+    // Delete Button
+    const delBtn = document.createElement('button');
+    delBtn.textContent = "Delete";
+    delBtn.setAttribute('data-action', 'delete');
+    delBtn.setAttribute('data-id', task.id);
+    li.appendChild(delBtn);
+
+    return li;
+}
